bb6cda80c716e3ccb09237a396a457ee03ff3445
Branko Stanković <banejecar@gmail.com>
2020-12-24 16:36:49 +0100

    boje

--- server_example/static/buildings.js
+++ server_example/static/buildings.js
@@ -1,6 +1,7 @@
 const buildings = {
   b1: {
     image: 'b1',
+    background: '#FFE0AB',
     tables: [
       { x: 100, y: 100, num: 2 },
       { x: 200, y: 100, num: 3 },
@@ -10,6 +11,7 @@ const buildings = {
   },
   b2: {
     image: 'b2',
+    background: '#FFDFD3',
     tables: [
       { x: 100, y: 100, num: 4 },
       { x: 200, y: 100, num: 3 },
@@ -20,6 +22,7 @@ const buildings = {
   },
   b3: {
     image: 'b3',
+    background: 'olive',
     tables: [
       { x: 100, y: 100, num: 4 },
       { x: 200, y: 100, num: 4 },
@@ -29,6 +32,7 @@ const buildings = {
   },
   b4: {
     image: 'b4',
+    background: '#957DAD',
     tables: [
       { x: 100, y: 100, num: 4 },
       { x: 200, y: 100, num: 2 },
@@ -40,6 +44,7 @@ const buildings = {
   },
   b5: {
     image: 'b5',
+    background: '#E0BBE4',
     tables: [
       { x: 100, y: 100, num: 4 },
       { x: 200, y: 100, num: 3 },
