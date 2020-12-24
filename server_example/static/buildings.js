38b190ab2f99aa4406d9006819bf891388617a61
Branko Stanković <banejecar@gmail.com>
2020-12-24 16:33:26 +0100

    pinovi Jelena

--- server_example/static/buildings.js
+++ server_example/static/buildings.js
@@ -1,47 +1,45 @@
 const buildings = {
   b1: {
     image: 'b1',
-    background: 'red',
     tables: [
-      { x: 100, y: 100, num: 4 },
+      { x: 100, y: 100, num: 2 },
       { x: 200, y: 100, num: 3 },
-      { x: 200, y: 200, num: 2 },
+      { x: 200, y: 200, num: 4 },
       { x: 300, y: 200, num: 2 },
     ]
   },
   b2: {
     image: 'b2',
-    background: 'blue',
     tables: [
       { x: 100, y: 100, num: 4 },
       { x: 200, y: 100, num: 3 },
       { x: 200, y: 200, num: 2 },
-      { x: 300, y: 200, num: 2 },
+      { x: 300, y: 200, num: 4 },
+      { x: 200, y: 300, num: 4 },
     ]
   },
   b3: {
     image: 'b3',
-    background: 'orange',
     tables: [
       { x: 100, y: 100, num: 4 },
-      { x: 200, y: 100, num: 3 },
+      { x: 200, y: 100, num: 4 },
       { x: 200, y: 200, num: 2 },
-      { x: 300, y: 200, num: 2 },
+      { x: 300, y: 200, num: 3 },
     ]
   },
   b4: {
     image: 'b4',
-    background: 'lime',
     tables: [
       { x: 100, y: 100, num: 4 },
-      { x: 200, y: 100, num: 3 },
-      { x: 200, y: 200, num: 2 },
+      { x: 200, y: 100, num: 2 },
+      { x: 200, y: 200, num: 3 },
       { x: 300, y: 200, num: 2 },
+      { x: 100, y: 300, num: 4 },
+      { x: 200, y: 300, num: 3 },
     ]
   },
   b5: {
     image: 'b5',
-    background: 'yellow',
     tables: [
       { x: 100, y: 100, num: 4 },
       { x: 200, y: 100, num: 3 },
